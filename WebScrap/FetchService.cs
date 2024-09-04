5c24e4f54a7b78b9147928a1eb0b16b425574e5e
onrdr <onrdrmn@gmail.com>
2024-09-04 05:00:13 +0300

    Add project files.

---
+++ WebScrap/FetchService.cs
@@ -0,0 +1,89 @@
+﻿using HtmlAgilityPack;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace WebScrap;
+
+public class FetchService
+{
+    public static void FetchData()
+    {
+        var pageCounter = 1;
+        var chromeDriver = new ChromeDriver();
+        chromeDriver.Manage().Window.Maximize();
+
+        var coreUrl = $"https://skinsort.com";
+
+        while (pageCounter <= 1383)
+        {
+            var siteUrl = $"{coreUrl}/ingredients/page/{pageCounter++}";
+
+            chromeDriver.Navigate().GoToUrl(siteUrl);
+
+            // Set up a wait to look for the button
+            var wait = new WebDriverWait(chromeDriver, TimeSpan.FromSeconds(10));
+
+            try
+            {
+                // Try to find and click the button if it appears
+                var button = wait.Until(driver => driver.FindElement(By.ClassName("dismiss-drawer-button")));
+                button.Click();
+                Console.WriteLine("Button clicked.");
+            }
+            catch (Exception e)
+            {
+                // Handle the case where the button does not appear
+                Console.WriteLine("Button not found within the timeout period.");
+            }
+
+            // Get the page source after the site has loaded
+            string pageSource = chromeDriver.PageSource;
+
+            // Create an HtmlDocument object and load the page source into it
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(pageSource);
+
+            var urlList = new List<string>();
+
+            var aTags = htmlDoc.DocumentNode.SelectNodes("//a[@class='w-1/2 lg:w-1/3 group cursor-pointer p-3']");
+
+            if (aTags != null)
+            {
+                foreach (var aTag in aTags)
+                {
+                    var hrefValue = aTag.GetAttributeValue("href", string.Empty);
+                    if (!string.IsNullOrEmpty(hrefValue))
+                    {
+                        urlList.Add($"{coreUrl}{hrefValue}");
+                    }
+                }
+            }
+
+
+            if (urlList is not null && urlList.Count != 0)
+            {
+                foreach (var link in urlList)
+                {
+                    chromeDriver.Navigate().GoToUrl(link);
+
+                    try
+                    {
+                        Thread.Sleep(3000);
+                        // Try to find and click the button if it appears
+                        var button = wait.Until(driver => driver.FindElement(By.ClassName("dismiss-drawer-button")));
+                        button.Click();
+                        Console.WriteLine("Button clicked.");
+                    }
+                    catch (Exception e)
+                    {
+                        // Handle the case where the button does not appear
+                        Console.WriteLine("Button not found within the timeout period.");
+                    }
+
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ WebScrap/Product.cs
@@ -0,0 +1,24 @@
+﻿namespace WebScrap;
+
+public class Product
+{
+    public int PageNumber { get; set; }
+    public string Name { get; set; }
+    public string Explained { get; set; }
+    public string WhatItIs { get; set; }
+    public string Benefits { get; set; }
+    public string Concerns { get; set; }
+    public string WhatItDoes { get; set; }
+    public string AlternativeNames { get; set; }
+    public CosIngData CosIngData { get; set; }
+}
+
+public class CosIngData
+{
+    public string CosIngID { get; set; }
+    public string INCIName { get; set; }
+    public string InnName { get; set; }
+    public string ECNumber { get; set; }
+    public string PhEurName { get; set; }
+    public string AllFunctions { get; set; }
+}
