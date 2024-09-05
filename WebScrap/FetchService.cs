d3ab9f7d74e4f7318355b73fae35c5fb542d5f14
onrdr <onrdrmn@gmail.com>
2024-09-05 18:26:49 +0300

    - Completed.

--- WebScrap/FetchService.cs
+++ WebScrap/FetchService.cs
@@ -14,7 +14,7 @@ public class FetchService
     public static void FetchData()
     {
         var productCounter = 1;
-        var pageCounter = 1;
+        var pageCounter = 269;
         var chromeDriver = new ChromeDriver();
         chromeDriver.Manage().Window.Maximize();
 
@@ -22,149 +22,173 @@ public class FetchService
 
         var productList = new List<Product>();
 
-        while (pageCounter <= 1383)
+        try
         {
-            var siteUrl = $"{coreUrl}/ingredients/page/{pageCounter}";
-
-            NavigateWithRetry(chromeDriver, siteUrl);
-
-            var wait = new WebDriverWait(chromeDriver, TimeSpan.FromSeconds(10));
-
-            try
+            while (pageCounter <= 1383)
             {
-                Thread.Sleep(3000);
-                var button = wait.Until(driver => driver.FindElement(By.ClassName("dismiss-drawer-button")));
-                button.Click();
-                Console.WriteLine("Button clicked.");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Button not found within the timeout period.");
-            }
+                var siteUrl = $"{coreUrl}/ingredients/page/{pageCounter}";
 
-            string pageSource = chromeDriver.PageSource;
-            var htmlDoc = new HtmlDocument();
-            htmlDoc.LoadHtml(pageSource);
+                NavigateWithRetry(chromeDriver, siteUrl);
 
-            var urlList = new List<string>();
+                var wait = new WebDriverWait(chromeDriver, TimeSpan.FromSeconds(10));
 
-            var aTags = htmlDoc?.DocumentNode?.SelectNodes("//a[@class='w-1/2 lg:w-1/3 group cursor-pointer p-3']");
-
-            if (aTags != null)
-            {
-                foreach (var aTag in aTags)
+                try
                 {
-                    var hrefValue = aTag.GetAttributeValue("href", string.Empty);
-                    if (!string.IsNullOrEmpty(hrefValue))
+                    Thread.Sleep(2000);
+                    var button = wait.Until(driver => driver.FindElement(By.ClassName("dismiss-drawer-button")));
+                    button.Click();
+                    Console.WriteLine($"Button clicked. Page: {pageCounter}");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Button not found within the timeout period.");
+                }
+
+                string pageSource = chromeDriver.PageSource;
+                var htmlDoc = new HtmlDocument();
+                htmlDoc.LoadHtml(pageSource);
+
+                var urlList = new List<string>();
+
+                var aTags = htmlDoc?.DocumentNode?.SelectNodes("//a[@class='w-1/2 lg:w-1/3 group cursor-pointer p-3']");
+
+                if (aTags != null)
+                {
+                    foreach (var aTag in aTags)
                     {
-                        urlList.Add($"{coreUrl}{hrefValue}");
+                        var hrefValue = aTag.GetAttributeValue("href", string.Empty);
+                        if (!string.IsNullOrEmpty(hrefValue))
+                        {
+                            urlList.Add($"{coreUrl}{hrefValue}");
+                        }
                     }
                 }
-            }
 
-            var web = new HtmlWeb();
+                var web = new HtmlWeb();
 
-            if (urlList is not null && urlList.Count != 0)
-            {
-                foreach (var link in urlList)
+                if (urlList is not null && urlList.Count != 0)
                 {
-                    var product = new Product();
-                    var cosIngData = new CosIngData();
-                    product.CosIngData = cosIngData;
-                    product.PageNumber = pageCounter;
-
-                    htmlDoc = web.Load(link);                   
-
-                    product.Name = HttpUtility.HtmlDecode(htmlDoc?.DocumentNode?.SelectSingleNode(".//article//h1")?.InnerText?.Trim());
-                    Console.WriteLine($"Product {productCounter++} Name: {product.Name}");
-
-                    var descriptionDiv = htmlDoc?.DocumentNode?.SelectSingleNode("//div[contains(@class, 'ingredient-description')]");
-                    if (descriptionDiv != null)
+                    foreach (var link in urlList)
                     {
-                        var paragraphs = descriptionDiv.SelectNodes(".//p");
-                        StringBuilder concatenatedText = new();
+                        var product = new Product();
+                        var cosIngData = new CosIngData();
+                        product.CosIngData = cosIngData;
+                        product.PageNumber = pageCounter;
 
-                        if (paragraphs != null)
+                        htmlDoc = web.Load(link);
+
+                        product.Name = HttpUtility.HtmlDecode(htmlDoc?.DocumentNode?.SelectSingleNode(".//article//h1")?.InnerText?.Trim());
+                        Console.WriteLine($"Product {productCounter++} Name: {product.Name}");
+
+                        var descriptionDiv = htmlDoc?.DocumentNode?.SelectSingleNode("//div[contains(@class, 'ingredient-description')]");
+                        if (descriptionDiv != null)
                         {
-                            foreach (var paragraph in paragraphs)
+                            var paragraphs = descriptionDiv.SelectNodes(".//p");
+                            StringBuilder concatenatedText = new();
+
+                            if (paragraphs != null)
                             {
-                                concatenatedText.Append(paragraph.InnerText.Trim() + " ");
+                                foreach (var paragraph in paragraphs)
+                                {
+                                    concatenatedText.Append(paragraph.InnerText.Trim() + " ");
+                                }
+                            }
+
+                            var explained = concatenatedText.ToString().Trim();
+                            var explainedDecoded = HttpUtility.HtmlDecode(explained);
+                            product.Explained = explainedDecoded;
+                        }
+
+                        var whatItIsDiv = htmlDoc?.DocumentNode?.SelectSingleNode("//div[@class='flex flex-wrap mt-1']");
+                        var whatItIsNodes = whatItIsDiv?.SelectNodes(".//div[@class='px-3 text-left py-0.5']");
+                        if (whatItIsNodes is not null && whatItIsNodes.Count != 0)
+                        {
+                            foreach (var whatItIs in whatItIsNodes)
+                            {
+                                product.WhatItIs.Add(HttpUtility.HtmlDecode(whatItIs.InnerText.Trim()));
                             }
                         }
 
-                        var explained = concatenatedText.ToString().Trim();
-                        var explainedDecoded = HttpUtility.HtmlDecode(explained);
-                        product.Explained = explainedDecoded;
-                    }
+                        var benefitAndConcernsDivs = htmlDoc?.DocumentNode?.SelectNodes("//div[@class='flex flex-wrap mt-2']");
 
-                    var whatItIsDiv = htmlDoc?.DocumentNode?.SelectSingleNode("//div[@class='flex flex-wrap mt-1']");
-                    var whatItIsNodes = whatItIsDiv?.SelectNodes(".//div[@class='px-3 text-left py-0.5']");
-                    if (whatItIsNodes is not null && whatItIsNodes.Count != 0)
-                    {
-                        foreach (var whatItIs in whatItIsNodes)
+                        if (benefitAndConcernsDivs is not null && benefitAndConcernsDivs.Count != 0)
                         {
-                            product.WhatItIs.Add(HttpUtility.HtmlDecode(whatItIs.InnerText.Trim()));
-                        }
-                    }
-
-                    var benefitAndConcernsDivs = htmlDoc?.DocumentNode?.SelectNodes("//div[@class='flex flex-wrap mt-2']");
-
-                    if (benefitAndConcernsDivs is not null && benefitAndConcernsDivs.Count != 0)
-                    {
-                        var benefitNodes = benefitAndConcernsDivs?.FirstOrDefault()?.SelectNodes(".//div[@class='px-3 text-left py-0.5']");
-                        if (benefitNodes is not null && benefitNodes.Count != 0)
-                        {
-                            foreach (var benefit in benefitNodes)
+                            var benefitNodes = benefitAndConcernsDivs?.FirstOrDefault()?.SelectNodes(".//div[@class='px-3 text-left py-0.5']");
+                            if (benefitNodes is not null && benefitNodes.Count != 0)
                             {
-                                product.Benefits.Add(HttpUtility.HtmlDecode(benefit.InnerText.Trim()));
+                                foreach (var benefit in benefitNodes)
+                                {
+                                    product.Benefits.Add(HttpUtility.HtmlDecode(benefit.InnerText.Trim()));
+                                }
                             }
                         }
-                    }
 
-                    if (benefitAndConcernsDivs is not null && benefitAndConcernsDivs.Count > 1)
-                    {
-                        var concernsNodes = benefitAndConcernsDivs?.LastOrDefault()?.SelectNodes(".//div[@class='px-3 text-left py-0.5']");
-                        if (concernsNodes is not null && concernsNodes.Count != 0)
+                        if (benefitAndConcernsDivs is not null && benefitAndConcernsDivs.Count > 1)
                         {
-                            foreach (var concern in concernsNodes)
+                            var concernsNodes = benefitAndConcernsDivs?.LastOrDefault()?.SelectNodes(".//div[@class='px-3 text-left py-0.5']");
+                            if (concernsNodes is not null && concernsNodes.Count != 0)
                             {
-                                product.Concerns.Add(HttpUtility.HtmlDecode(concern.InnerText.Trim()));
+                                foreach (var concern in concernsNodes)
+                                {
+                                    product.Concerns.Add(HttpUtility.HtmlDecode(concern.InnerText.Trim()));
+                                }
                             }
                         }
-                    }
 
-                    var whatItDoesNodes = htmlDoc?.DocumentNode?.SelectNodes("//span[@class='text-xs text-warm-gray-700 py-2 font-normal']");
-                    if (whatItDoesNodes is not null && whatItDoesNodes.Count != 0)
-                    {
-                        foreach (var whatItDoes in whatItDoesNodes)
+                        var whatItDoesNodes = htmlDoc?.DocumentNode?.SelectNodes("//div[contains(@class, 'border-b lg:border-none lg:mb-2 cursor-pointer')]");
+                        if (whatItDoesNodes is not null && whatItDoesNodes.Count != 0)
                         {
-                            product.WhatItDoes.Add(HttpUtility.HtmlDecode(whatItDoes.InnerText.Trim()));
-                        }
-                    }
+                            foreach (var container in whatItDoesNodes)
+                            {
+                                var headingNode = container.SelectSingleNode(".//text()");
+                                var headingText = headingNode != null ? HttpUtility.HtmlDecode(headingNode.InnerText.Trim()) : "";
 
-                    var alternativeNamesDiv = htmlDoc?.DocumentNode?.SelectNodes("//div[@class='border-b lg:border-none lg:mb-2 lg:text-sm lg:bg-warm-gray-100 lg:rounded-xl border-warm-gray-100 py-2.5 px-4 font-medium']");
-                    if (alternativeNamesDiv is not null && alternativeNamesDiv.Count != 0)
-                    {
-                        foreach (var alternativeName in alternativeNamesDiv)
+                                var descriptionNode = container.SelectSingleNode(".//span[@class='text-xs text-warm-gray-700 py-2 font-normal']");
+                                var descriptionText = descriptionNode != null ? HttpUtility.HtmlDecode(descriptionNode.InnerText.Trim()) : "";
+                                var whatItDoes = string.Empty;
+
+                                if (!string.IsNullOrEmpty(headingText))
+                                {
+                                    whatItDoes += ($"{headingText}");
+                                }
+
+                                if (!string.IsNullOrEmpty(descriptionText))
+                                {
+                                    whatItDoes += ($": {descriptionText}");
+                                }
+
+                                product.WhatItDoes.Add(whatItDoes);
+                            }
+                        }
+
+                        var alternativeNamesDiv = htmlDoc?.DocumentNode?.SelectNodes("//div[@class='border-b lg:border-none lg:mb-2 lg:text-sm lg:bg-warm-gray-100 lg:rounded-xl border-warm-gray-100 py-2.5 px-4 font-medium']");
+                        if (alternativeNamesDiv is not null && alternativeNamesDiv.Count != 0)
                         {
-                            product.AlternativeNames.Add(HttpUtility.HtmlDecode(alternativeName.InnerText.Trim()));
+                            foreach (var alternativeName in alternativeNamesDiv)
+                            {
+                                product.AlternativeNames.Add(HttpUtility.HtmlDecode(alternativeName.InnerText.Trim()));
+                            }
                         }
+
+                        var cosIngDataDiv = htmlDoc?.DocumentNode?.SelectSingleNode("//div[@class='rounded-xl flex flex-col bg-white mb-20 lg:max-w-md']");
+
+                        cosIngData.CosIngID = HttpUtility.HtmlDecode(cosIngDataDiv?.SelectSingleNode(".//div[contains(., 'CosIng ID')]/span")?.InnerText?.Trim());
+                        cosIngData.INCIName = HttpUtility.HtmlDecode(cosIngDataDiv?.SelectSingleNode(".//div[contains(., 'INCI Name')]/span")?.InnerText?.Trim());
+                        cosIngData.InnName = HttpUtility.HtmlDecode(cosIngDataDiv?.SelectSingleNode(".//div[contains(., 'INN Name')]/span")?.InnerText?.Trim());
+                        cosIngData.ECNumber = HttpUtility.HtmlDecode(cosIngDataDiv?.SelectSingleNode(".//div[contains(., 'EC #')]/span")?.InnerText?.Replace("&nbsp;", "")?.Trim());
+                        cosIngData.PhEurName = HttpUtility.HtmlDecode(cosIngDataDiv?.SelectSingleNode(".//div[contains(., 'Ph. Eur. Name')]/span")?.InnerText?.Trim());
+                        cosIngData.AllFunctions = HttpUtility.HtmlDecode(cosIngDataDiv?.SelectSingleNode(".//div[contains(., 'All Functions')]/span")?.InnerText?.Trim());
+
+                        productList.Add(product);
                     }
-
-                    var cosIngDataDiv = htmlDoc?.DocumentNode?.SelectSingleNode("//div[@class='rounded-xl flex flex-col bg-white mb-20 lg:max-w-md']");
-
-                    cosIngData.CosIngID = HttpUtility.HtmlDecode(cosIngDataDiv?.SelectSingleNode(".//div[contains(., 'CosIng ID')]/span")?.InnerText?.Trim());
-                    cosIngData.INCIName = HttpUtility.HtmlDecode(cosIngDataDiv?.SelectSingleNode(".//div[contains(., 'INCI Name')]/span")?.InnerText?.Trim());
-                    cosIngData.InnName = HttpUtility.HtmlDecode(cosIngDataDiv?.SelectSingleNode(".//div[contains(., 'INN Name')]/span")?.InnerText?.Trim());
-                    cosIngData.ECNumber = HttpUtility.HtmlDecode(cosIngDataDiv?.SelectSingleNode(".//div[contains(., 'EC #')]/span")?.InnerText?.Replace("&nbsp;", "")?.Trim());
-                    cosIngData.PhEurName = HttpUtility.HtmlDecode(cosIngDataDiv?.SelectSingleNode(".//div[contains(., 'Ph. Eur. Name')]/span")?.InnerText?.Trim());
-                    cosIngData.AllFunctions = HttpUtility.HtmlDecode(cosIngDataDiv?.SelectSingleNode(".//div[contains(., 'All Functions')]/span")?.InnerText?.Trim());
-
-                    productList.Add(product);
                 }
-            }
 
-            pageCounter++;
+                pageCounter++;
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Exception thrown in page: {pageCounter}\nMessage: {e.Message} ");
         }
 
         var options = new JsonSerializerOptions
